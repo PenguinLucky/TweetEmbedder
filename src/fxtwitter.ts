3c779d465a60db3264f306bcd53b6e4621c1e3e9
PenguinLucky <45398456+PenguinLucky@users.noreply.github.com>
2023-11-03 06:00:49 +0900

    Add content-type validation for API responses

--- src/fxtwitter.ts
+++ src/fxtwitter.ts
@@ -3,6 +3,7 @@ import { fxtwitter } from "./types/fxtwitter";
 export async function getTweet(tweetId: string, authorId: string): Promise<fxtwitter.Tweet | null> {
     const response = await fetch(`https://api.fxtwitter.com/${authorId}/status/${tweetId}`);
     if (!response.ok) return null;
+    if (response.headers.get("content-type")?.startsWith("application/json") !== true) return null;
     const json = (await response.json()) as fxtwitter.TweetResponse;
     if (json.code !== 200) return null;
     return json.tweet ?? null;
@@ -11,6 +12,7 @@ export async function getTweet(tweetId: string, authorId: string): Promise<fxtwi
 export async function getUser(userId: string): Promise<fxtwitter.User | null> {
     const response = await fetch(`https://api.fxtwitter.com/${userId}`);
     if (!response.ok) return null;
+    if (response.headers.get("content-type")?.startsWith("application/json") !== true) return null;
     const json = (await response.json()) as fxtwitter.UserResponse;
     if (json.code !== 200) return null;
     return json.user ?? null;
